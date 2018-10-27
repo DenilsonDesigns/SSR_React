827697b0bccf21dd50b452b12be3c739c76288b8
DenilsonDesigns <daniellanguilller@gmail.com>
2018-10-27 23:44:32 +1100

    webpack

--- build/bundle.js
+++ build/bundle.js
@@ -8906,14 +8906,26 @@ module.exports = memoizeStringOnly;
 "use strict";
 
 
-var express = __webpack_require__(68);
-var React = __webpack_require__(26);
-var renderToString = __webpack_require__(147).renderToString;
-var Home = __webpack_require__(156).default;
-var app = express();
+var _express = __webpack_require__(68);
+
+var _express2 = _interopRequireDefault(_express);
+
+var _react = __webpack_require__(26);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(147);
+
+var _Home = __webpack_require__(156);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
 
 app.get("/", function (req, res) {
-  var content = renderToString(React.createElement(Home, null));
+  var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
   res.send(content);
 });
@@ -27798,7 +27810,18 @@ var Home = function Home() {
   return _react2.default.createElement(
     "div",
     null,
-    "I'm the home component"
+    _react2.default.createElement(
+      "div",
+      null,
+      "I'm the BEST home component"
+    ),
+    _react2.default.createElement(
+      "button",
+      { onClick: function onClick() {
+          return console.log("Hi there");
+        } },
+      "Press me!"
+    )
   );
 };
 
